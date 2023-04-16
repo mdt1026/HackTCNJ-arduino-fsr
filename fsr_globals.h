b102646d5d90156ee4af5ff7b399e5c3329b20ad
mdt1026 <mdturner1026@gmail.com>
2023-04-16 01:26:01 -0400

    Fixed multiple definition of 'WMA_MAX_WIDTH' compilation error

--- fsr_globals.h
+++ fsr_globals.h
@@ -6,8 +6,6 @@
 const long BAUD_RATE = 57600;
 const uint16_t DEFAULT_THRESHOLD = 50;
 const size_t MAX_SHARED_SENSORS = 2;
-extern const size_t NUM_FSRS;
-extern const size_t WMA_MAX_WIDTH;
 
 extern uint8_t current_button;
 
