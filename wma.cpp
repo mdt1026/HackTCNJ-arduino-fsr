a536ea96e52dab93e198f36fb6ed7b12ebad4654
mdt1026 <mdturner1026@gmail.com>
2023-04-16 05:42:17 -0400

    Fix for issue with moving_average smoothing

--- wma.cpp
+++ wma.cpp
@@ -1,18 +1,33 @@
+#include "Arduino.h"
 #include "wma.h"
 extern const size_t WMA_MAX_WIDTH;
 
 WeightedMovingAverage::WeightedMovingAverage(size_t width) : _width(min(width, WMA_MAX_WIDTH)), _sum(0), _weighted(0), _values{}, _count(0) {}
 
-uint16_t WeightedMovingAverage::get_average(uint16_t value) {
+int16_t WeightedMovingAverage::get_average(uint16_t value) {
   // Calculate sum with current value and remove oldest value
-  uint32_t next = _sum - _values[_count] + value;
+  // Serial.println(_sum);
+  int32_t next = _sum + value - _values[_count];
+  // Serial.print("next: ");
+  // Serial.println(value);
   // Calculate weighted sum with current value
-  uint32_t next_weighted = _weighted - _sum + (value * _width);
+  int32_t next_weighted = _weighted + (value * _width) - _sum;
+  // Serial.print("next_weighted: ");
+  // Serial.println(value);
   _sum = next;
   _weighted = next_weighted;
   _values[_count] = value;
   _count = (_count + 1) % _width;
+  // Serial.print("sum | ");
+  // Serial.println(_sum);
+  // Serial.print("weighted | ");
+  // Serial.println(_weighted);
+  // Serial.print("count | ");
+  // Serial.println(_count);
   // Sum of weights = sum of integers from [1, _size]
-  uint16_t sum_weights = ((_width* (_width + 1)) / 2);
+  int16_t sum_weights = ((_width * (_width + 1)) / 2);
+  // Serial.print("sum_weights | ");
+  // Serial.println(sum_weights);
+  // Serial.println();
   return next_weighted / sum_weights;
 }
