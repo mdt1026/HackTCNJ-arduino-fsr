d8744fef962e17ab008afe99c2580eff7bb7aa92
g00gol <jackey2870@gmail.com>
2023-04-15 20:02:23 -0400

    threejs working

--- web-ui/components/MainScene.tsx
+++ web-ui/components/MainScene.tsx
@@ -1,7 +1,11 @@
 import * as THREE from "three";
-import { useEffect, useRef } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { useEffect, useRef, useState } from "react";
 
 export default function MainScene() {
+  const [] = useState(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
@@ -17,34 +21,116 @@ export default function MainScene() {
       return;
     }
 
-    // Initialize the scene, camera, and renderer
+    /**
+     * Initialize the scene, camera, light, and renderer
+     */
+
+    // Scene
     const scene = new THREE.Scene();
+
+    // Camera
     const camera = new THREE.PerspectiveCamera(
-      75,
+      20,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
-    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
+
+    // Light
+    const ambientLight = new THREE.AmbientLight(0x404040, 10);
+    scene.add(ambientLight);
+
+    // Add a point light to the scene
+    const pointLight = new THREE.PointLight(0xffffff, 5, 10);
+    pointLight.position.set(10, 10, 10);
+    scene.add(pointLight);
+
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      canvas: canvasRef.current,
+    });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
 
-    // Add a cube to the scene
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    // Set the background color of the scene to white
+    renderer.setClearColor(0xffffff);
+
+    // Load our assets
+    const dracoLoader = new DRACOLoader();
+    const loader = new GLTFLoader();
+
+    dracoLoader.setDecoderPath("/draco/");
+    loader.setDRACOLoader(dracoLoader);
+
+    let desiredChild: THREE.Object3D[] = [];
+
+    loader.load(
+      "/models/main.glb",
+      (gltf) => {
+        console.log("Loaded GLB file", gltf.scene);
+        scene.add(gltf.scene);
+
+        for (let child of gltf.scene.children) {
+          if (
+            child.name === "PadLeft" ||
+            child.name === "PadTop" ||
+            child.name === "PadRight" ||
+            child.name === "PadBottom"
+          ) {
+            desiredChild.push(child);
+          }
+        }
+      },
+      undefined,
+      (error) => {
+        console.error("An error occurred while loading the GLB file", error);
+      }
+    );
+
+    // Instantiate a Raycaster and a Vector2 for the mouse position
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
+
+    // Function to handle mouse clicks
+    function onClick(event: MouseEvent) {
+      event.preventDefault();
+
+      // Calculate the mouse position in normalized device coordinates (-1 to +1)
+      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      // Update the picking ray with the camera and mouse position
+      raycaster.setFromCamera(mouse, camera);
+
+      // Calculate objects intersecting the picking ray
+      const intersects = raycaster.intersectObjects(scene.children, true);
+
+      // Check if the desired child object is intersected and log "hello world"
+      for (let intersect of intersects) {
+        if (desiredChild.includes(intersect.object)) {
+          console.log(intersect);
+          break;
+        }
+      }
+    }
+
+    // Add event listener for mouse clicks
+    window.addEventListener("click", onClick);
 
     // Camera Position
-    camera.position.z = 5;
+    camera.position.set(5, 5, 5);
+
+    // Add OrbitControls to enable zoom functionality
+    const controls = new OrbitControls(camera, canvasRef.current);
+    controls.enableZoom = true;
+    controls.enablePan = false;
+    controls.enableDamping = true;
+    controls.update();
 
     // Animate the cube
     function animate() {
       requestAnimationFrame(animate);
-
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
       renderer.render(scene, camera);
     }
     animate();
@@ -52,7 +138,7 @@ export default function MainScene() {
 
   return (
     <div className="h-screen w-screen">
-      <canvas ref={canvasRef} className="h-full w-full"></canvas>
+      <canvas ref={canvasRef} className="h-full w-full bg-white"></canvas>
     </div>
   );
 }
