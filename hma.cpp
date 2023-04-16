4e1ab8ed44ba8666d986bd5dd01233d265259c68
mdt1026 <mdturner1026@gmail.com>
2023-04-16 05:42:17 -0400

    Fix for issue with moving_average smoothing

--- hma.cpp
+++ hma.cpp
@@ -1,11 +1,14 @@
 #include <inttypes.h>
+#include "Arduino.h"
 #include "hma.h"
 
 HullMovingAverage::HullMovingAverage(size_t size) : _wma1(size/2), _wma2(size), _hull(sqrt(size)) {}
 
-uint16_t HullMovingAverage::get_average(uint16_t value) {
-  uint16_t wma1_value = _wma1.get_average(value);
-  uint16_t wma2_value = _wma2.get_average(value);
-  uint16_t hull_value = _hull.get_average(2 * (wma1_value - (wma2_value/2)));
-  return hull_value;
+int16_t HullMovingAverage::get_average(uint16_t value) {
+  int16_t wma1_value = _wma1.get_average(value);
+  int16_t wma2_value = _wma2.get_average(value);
+  int16_t hull_value = _hull.get_average(max(2 * wma1_value - wma2_value, 0));
+  // Serial.println(hull_value);
+  // return hull_value;
+  return constrain(hull_value, 0, 1023);
 }
