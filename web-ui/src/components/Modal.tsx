484ea6a34c050f68d9350587c24df3bc6c2bd5d9
g00gol <jackey2870@gmail.com>
2023-04-16 10:05:57 -0400

    design

--- web-ui/src/components/Modal.tsx
+++ web-ui/src/components/Modal.tsx
@@ -11,13 +11,13 @@ const Modal: React.FC<ModalProps> = ({ pads, setToggleModalCallback }) => {
   const uniquePads = [...new Set(pads)];
   const [padsData, setPadsData] = useState<any[]>([]);
   const [def, setDef] = useState<any[]>([]);
+  const modalRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const ws = new WebSocket("ws://0.0.0.0:5000/ws");
 
     ws.onopen = () => {
       console.log("WebSocket connection established");
-      ws.send("Hello, WebSocket!");
     };
 
     ws.onmessage = (event: MessageEvent) => {
@@ -32,10 +32,35 @@ const Modal: React.FC<ModalProps> = ({ pads, setToggleModalCallback }) => {
     ws.onclose = () => {
       console.log("WebSocket connection closed");
     };
+
+    const handleMouseMove = (event: MouseEvent) => {
+      if (modalRef.current) {
+        const centerX = modalRef.current.offsetWidth / 2;
+        const centerY = modalRef.current.offsetHeight / 2;
+        const mouseX = event.clientX - modalRef.current.offsetLeft - centerX;
+        const mouseY = event.clientY - modalRef.current.offsetTop - centerY;
+        const tiltX = -(mouseY / centerY) * 10;
+        const tiltY = (mouseX / centerX) * 10;
+        modalRef.current.style.transform = `perspective(1000px) rotateX(${tiltX}deg) rotateY(${tiltY}deg)`;
+      }
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
   }, []);
 
   return (
-    <div className="absolute right-[4vw] h-fit min-h-[70%] w-6/12 bg-white p-8">
+    <div
+      ref={modalRef}
+      className="absolute right-[4vw] h-fit min-h-[70%] w-6/12 transform bg-white p-8 transition-transform duration-500"
+      style={{
+        transformStyle: "preserve-3d",
+        boxShadow: "0px 0px 20px #333333",
+      }}
+    >
       <AiOutlineClose
         onClick={() => setToggleModalCallback(false)}
         className="absolute right-0 top-0 m-4"
@@ -67,7 +92,6 @@ const Modal: React.FC<ModalProps> = ({ pads, setToggleModalCallback }) => {
               : 0}
           </p>
           <label htmlFor="padData">
-            Force:{" "}
             {padsData[0] === "values"
               ? pad === "PadLeft"
                 ? padsData[1].values[0]
@@ -81,7 +105,7 @@ const Modal: React.FC<ModalProps> = ({ pads, setToggleModalCallback }) => {
               : 0}
           </label>
           <input
-            className="w-full"
+            className="pbbb-4 w-full rounded-md border-0 shadow-xl outline-none"
             name="padData"
             type="range"
             min="1"
